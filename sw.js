699c5ab65344514db0ce176406d999aac595c5cd
ahmeddkhalaf <ak5987440@gmail.com>
2026-07-06 13:32:33 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'med-countdown-v14';
+const ASSETS_TO_CACHE = [
+  'index.html',
+  'manifest.json',
+  'tabeeb.mp3',
+  'https://i.ibb.co/JFjmFfnG/IMG-20260516-WA0015.jpg'
+];
+
+// Instantly download and cache assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Caching assets for offline capability...');
+      return cache.addAll(ASSETS_TO_CACHE);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+// Clear old caches if updates happen later
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Serve local assets directly when phone is offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
